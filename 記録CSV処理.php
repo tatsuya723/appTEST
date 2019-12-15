0684bb1f55fd5427910337bc1e6649a1891739c6
T.Okabayashi <e5408@gm.kochi-ct.jp>
2019-12-16 05:12:50 +0900

    no message

--- 記録CSV処理.php
+++ 記録CSV処理.php
@@ -1,28 +1,58 @@
 <?php
-require_once("DB接続.php");
-$pdo=db_connect();
+/*▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲
+    //DB接続処理
+▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲*/
+$dbhost="ec2-174-129-255-46.compute-1.amazonaws.com";
+$dbname="dflv6jh505d9tv";
+$dbuser="qajdgcrnucpdpx";
+$dbpass="d2144f11fa2bc512c9f5f4d65cef0b1f804fabef86759d786bd6ca430eba6fa8";
+$dbtype="pgsql";
+$dsn = "$dbtype:dbname=$dbname host=$dbhost port=5432";
+try{
+    $pdo=new PDO($dsn,$dbuser,$dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+    //print"接続しました<br>";
+}catch(PDOException $Exception){
+    die('エラー:'.$Exception->getMessage());}
+/*▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲
+▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲*/
+
 
 header("Content-Type: application/octet-stream");
 header("Content-Disposition: attachment; filename=GRAYCODE.csv");
 header("Content-Transfer-Encoding: binary");
 
+//key1:処理区別番号
+//key21:名前
+//key31:年
+//key32:月
+//key33:日
+$key21=$_POST['key21'];
+$Y=$_POST['key31'];
+$M=$_POST['key32'];
+$D=$_POST['key33'];
 
-//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーー名前(orカードID,作業内容)と年月日すべて指定ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーー指定：[名前、年、月、日]ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 if($_POST['key1']=="11"){
-	$search_key='%'. $_POST['key2'].'%';
-	//$D=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
-	$sql="SELECT * FROM work2 WHERE ( cardID like :cardID OR member like :member OR work like :work ) AND ( year = :year AND month = :month AND day = :day)";
-	$stmh=$pdo->prepare($sql);
-	$stmh->bindValue(':cardID',$search_key,PDO::PARAM_STR);
-	$stmh->bindValue(':member',$search_key,PDO::PARAM_STR);
-	$stmh->bindValue(':work',$search_key,PDO::PARAM_STR);
-	$stmh->bindValue(':year',$_POST['key21'],PDO::PARAM_STR);
-	$stmh->bindValue(':month',$_POST['key22'],PDO::PARAM_STR);
-	$stmh->bindValue(':day',$_POST['key23'],PDO::PARAM_STR);
-	$stmh->execute();
+	$tabname="b_".$Y."_".$M;
+    $tabsel="SELECT * FROM ".$tabname;
+    try{
+    $stmh=$pdo->query($tabsel);
+    $stmh->execute();
+    }catch(PDOException $Exception){
+        print "エラー:".$Exception->getMessage();
+    }
+	$rs = $stmh->fetchall ();
 
+	for($i=0;$i<;$i++){
+		if($rs[i]['']$key21){
 
-//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーー名前(orカードID,作業内容)と年月指定ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
+		}
+
+	}
+
+//ーーーーーーーーーーーーーーーーーーーーーーーーーーーーー指定：[名前、年、月]ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
 }elseif($_POST['key1']=="12"){
 	$search_key='%'. $_POST['key2'].'%';
 	//$D=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
@@ -106,10 +136,14 @@ if($_POST['key1']=="11"){
 	
 
 
-$csv = '"",カードID","氏名","レーン","作業時間(分)","作業内容","年","月","日"' . "\n";
 
-foreach( $stmh as $value ) {
-	$csv .= "".",". $value['cardID']  .",".  $value['member'] .",".  $value['rane'] .",". $value['time'] .",". $value['work'].",". $value['year'].",".$value['month'].",".$value['day'].  "\n";
+
+/*▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲
+    //CSVファイル作成
+▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲*/
+$csv = '"","カードID","氏名","作業時間[分]","作業内容","レーン","年月日","時刻"' . "\n";
+foreach ( $rs as $row ) {
+	$csv .= "".",". $row['card_id']  .",".  $value['member'] .",".  $value['work_time'] .",". $value['work'] .",". $value['rane'].",". $value['d_ymd'].",".$value['dt']. "\n";
 }
 
 mb_convert_variables('Shift_JIS' , 'UTF-8' , $csv );
