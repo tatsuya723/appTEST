ba9bb724a307055d8d2b51b5cc616e05a1180a37
T.Okabayashi <e5408@gm.kochi-ct.jp>
2019-12-20 08:36:37 +0900

    no message

--- 記録CSV処理.php
+++ 記録CSV処理.php
@@ -198,7 +198,7 @@ if($_POST['key1']=="11"){
 	mb_convert_variables("UTF-8", "SJIS", $csv[$i]['d_ymd']);
 	mb_convert_variables("UTF-8", "SJIS", $csv[$i]['dt']);
 */}
-mb_convert_encoding('ANSI' , 'UTF-8' , $csv );
+//mb_convert_encoding('ANSI' , 'UTF-8' , $csv );
 echo $csv;
 return;
 
