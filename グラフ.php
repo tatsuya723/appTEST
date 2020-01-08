0bb6724fd63fb742493df6b6398da3456e5d3cc3
T.Okabayashi <e5408@gm.kochi-ct.jp>
2020-01-09 01:29:05 +0900

    no message

--- グラフ.php
+++ グラフ.php
@@ -1,24 +1,28 @@
+	
 <?php
-// ダウンロードした以下のファイルを読み込む
-require_once ('jpgraph.php');
-require_once ('jpgraph_bar.php');
- 
-// 縦軸のデータ
-$x_data = array(1,5,10);
- 
-// グラフの生成
-$graph = new Graph(400, 300);
-$graph->SetScale('textlin');
- 
-$graph->SetMarginColor('white');
- 
-// タイトル
-$graph->title->Set('samurai_graph');
- 
-// グラフ表示
-$bar = new BarPlot($x_data);
-$bar->value->Show();
-$graph->Add($bar);
-$graph->Stroke();
- 
-?>
+// 1円グラフ描画に必要なライブラリ
+require_once 'jpgraph-4.2.10/src/jpgraph.php';
+require_once 'jpgraph-4.2.10/src/jpgraph_pie.php';
+
+// データ、凡例、描画色を準備
+$data = array(
+  'data' => array(3125, 2981, 1846, 1124, 1007),
+  'legends' => array('駅前店','本通店','胡町点','春日店','元町店'),
+  'colors' => array('#0000FF', '#6600FF', '#CC00FF', '#66CC00', '#FFCC00')
+);
+
+// 2円グラフを準備
+$pie = new PiePlot($data['data']); // データ
+$pie->setLegends($data['legends']); // 凡例
+$pie->setSize(0.4); // サイズ
+$pie->setSliceColors($data['colors']); // 描画色
+
+// 3グラフの描画先
+$g = new PieGraph(400,400); // サイズ
+$g->title->setFont(FF_MINCHO, FS_NORMAL, 14); // タイトルフォント
+$g->legend->setFont(FF_MINCHO, FS_NORMAL, 14); // 凡例フォント
+$g->title->set('店舗別売上高'); // タイトル
+$g->add($pie); // グラフを追加
+
+// 4グラフを描画
+$g->stroke();
