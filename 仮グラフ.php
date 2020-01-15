22855906916d3029cbd227cf848a032a177a3e63
T.Okabayashi <e5408@gm.kochi-ct.jp>
2020-01-16 00:41:47 +0900

    no message

--- 仮グラフ.php
+++ 仮グラフ.php
@@ -86,14 +86,6 @@
 </form>
 
 <?php
-$MEMBER[0]="島井";
-$MEMBER[1]="都築";
-$MEMBER[2]="西本";
-$MEMBER[3]="濱田";
-$MEMBER[4]="広瀬";
-$MEMBER[5]="吉川";
-$MEMBER[6]="土居";
-$MEMBER[7]="山中";
 $DAY = (string) $_POST["day"];
 /*▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲
     //DB接続処理
@@ -143,7 +135,6 @@ foreach ( $sample_member as $member_b ) {
 /*▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲
     //検索条件に応じて、グラフの値を取得する
 ▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲*/
-
 $Y=$_POST["year"];
 $M=$_POST["month"];
 if($Y!="" && $M!=""){
@@ -159,18 +150,20 @@ if($Y!="" && $M!=""){
   $data = $stmh->fetchall ();
   for($a=0;$a<count($member);$a++){
     $sum_time=0;//合計時間の初期化
+    $sum_eff=0;//効率の合計初期化
+    $count_eff=0;//効率の平均を求めるときに使う値
     foreach ( $data as $wtime ) {
-      if($wtime["member"]==$member[$a]){ //$member[]の名前と一致したら
-        $fl_time=(float)$wtime["work_time"];
-        $sum_time+=$fl_time;
+      if($wtime["member"]==$member[$a]){      //$member[]の名前と一致したら
+        $fl_time=(float)$wtime["work_time"];  //float型に変換
+        $sum_time+=$fl_time;                  //作業時間を足していく
+        $fl_eff=(float)$wtime["eff"];         //float型に変換
+        $sum_eff=+=$fl_eff;                   //作業効率値を足していく
+        $count_eff+=1;
       }      
     }
-    $sum_worktime[$a]=$sum_time;  
+    $sum_worktime[$a]=$sum_time;              //作業時間の合計を配列に保存
+    $ave_eff[$a]=($sum_eff/$count_eff);       //効率の平均値を配列に保存
   }
-  print $sum_worktime;
-
-/*▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲
-▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲▼▲*/
 ?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
@@ -178,17 +171,12 @@ if($Y!="" && $M!=""){
 <hr size="5" noshade>
 <hr size="5" noshade>
 <hr size="5" noshade>
-<hr size="5" noshade>
-<hr size="5" noshade>
-<hr size="5" noshade>
 <h2 color="#ffa500">作業時間の合計</h2>
 <hr size="5" noshade>
-<font size="4" color="#ff0000">2020年1月</font><br>
+<font size="4" color="#ff0000"><?=$Y?>年<?=$M?>月</font><br>
 <canvas id="作業時間合計" width="20" height="5"></canvas><br><br>
-<br><br><br><br><br><br><br><br><br><br><br>
-<hr size="5" noshade>
-<hr size="5" noshade>
-<hr size="5" noshade>
+<br><br><br><br><br><br>
+
 <hr size="5" noshade>
 <hr size="5" noshade>
 <hr size="5" noshade>
@@ -200,21 +188,19 @@ if($Y!="" && $M!=""){
 <script type="text/javascript">
 var member = <?php echo json_encode($member); ?>;
 var sum_worktime = <?php echo json_encode($sum_worktime); ?>;
-
+var ave_eff = <?php echo json_encode($ave_eff); ?>;
 var work_time = document.getElementById('作業時間合計').getContext('2d');
 var myChart1 = new Chart(work_time, {
-    type: 'bar',
-    data: {
-        labels: member,
-        datasets: [{
-        label: '作業時間合計',
-        data: sum_worktime,
-        backgroundColor: "rgba(20,255,0,0.9)"
-        
-        }]
-    }   
+  type: 'bar',
+  data: {
+    labels: member,
+    datasets: [{
+      label: '作業時間合計',
+      data: sum_worktime,
+      backgroundColor: "rgba(20,255,0,0.9)"    
+    }]
+  }   
 });
-
 var work_time2 = document.getElementById('平均作業効率').getContext('2d');
 var myChart1 = new Chart(work_time2, {
   type: 'bar',
@@ -222,7 +208,7 @@ var myChart1 = new Chart(work_time2, {
     labels: member,
     datasets: [{
       label: '平均作業効率',
-      data: [84,81,77,92,77,88,79,82],
+      data: ave_eff,
       backgroundColor: "rgba(20,255,0,0.9)"
     }]
   }
