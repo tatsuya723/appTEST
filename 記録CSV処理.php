4a5dfe212e9463e70b850ddbd5a3c3696904b6e2
T.Okabayashi <e5408@gm.kochi-ct.jp>
2020-01-16 03:00:59 +0900

    no message

--- 記録CSV処理.php
+++ 記録CSV処理.php
@@ -213,7 +213,7 @@ if($_POST['key1']=="11"){
 	mb_convert_variables("UTF-8", "SJIS", $csv[$i]['d_ymd']);
 	mb_convert_variables("UTF-8", "SJIS", $csv[$i]['dt']);
 */
-mb_convert_encoding($csv,"SJIS","UTF-8");
+mb_convert_variables('Shift_JIS' , 'UTF-8' , $csv );
 echo $csv;
 return;
 
