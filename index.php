5f25e6e379b29b1b2cb4a79f9b4442ff455f6a3e
T.Okabayashi <e5408@gm.kochi-ct.jp>
2020-01-18 10:51:42 +0900

    no message

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ try{
     if((isset($_SESSION["pass"])) && ($_SESSION["pass"]==$pass)){
     ?>
         <a href="" onclick="document.sagyou.submit();return false;"><font size="5">作業記録管理ページ</font></a><br><br>
-        <a href="" onclick="document.sagyou.submit();return false;"><font size="5">カード管理ページ</font></a>
+        <a href="" onclick="document.card.submit();return false;"><font size="5">カード管理ページ</font></a>
         <form method="post" name="sagyou" action="作業記録.php">
         <input type="hidden" name="pass" value=<?=$_SESSION["pass"]?>>
         </form>
